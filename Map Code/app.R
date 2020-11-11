dc58b9e8e59cea289411980fac662eb61120786f
Steedman Jenkins <sjenkins23@amherst.edu>
2020-11-10 22:22:21 -0500

    figured out long form!

--- Map Code/app.R
+++ Map Code/app.R
@@ -4,15 +4,48 @@ library(viridis)
 library(maps)
 library(leaflet)
 library(shinythemes)
-library(tidyverse)
 library(rvest)
 library(robotstxt)
 library(tidycensus)
 library(tidytext)
 library(shiny)
 
-path_in <- "/Users/rodrigo/Git/Blog-Data-For-Good"
+path_in <- "/Users/steedmanjenkins/git/Blog-Data-For-Good"
 city_data <- read_csv(paste0(path_in, "/dataset.csv"))
+
+#put data in long form
+common_vars <- colnames(city_data[c(1:3, 21:27)])
+short_vars <- c("median_age_male", "median_age_female", "population", "white_alone", "black_alone",
+                "foreign_born", "vehicles_avail", "total_hh", "married_hh", "male_bachelors", "female_bachelors",
+                "poverty", "median_income", "hours_worked", "houses_for_sale", "median_value", "speaks_only_english")
+
+data13 <- city_data%>%
+  select(c(common_vars, str_which(colnames(city_data), "2013")))%>%mutate(year = 2013)
+colnames(data13)<-c(common_vars, short_vars, "year")
+
+data14 <- city_data%>%
+  select(c(common_vars, str_which(colnames(city_data), "2014")))%>%mutate(year = 2014)
+colnames(data14)<-c(common_vars, short_vars, "year")
+
+data15 <- city_data%>%
+  select(c(common_vars, str_which(colnames(city_data), "2015")))%>%mutate(year = 2015)
+colnames(data15)<-c(common_vars, short_vars, "year")
+
+data16 <- city_data%>%
+  select(c(common_vars, str_which(colnames(city_data), "2016")))%>%mutate(year = 2016)
+colnames(data16)<-c(common_vars, short_vars, "year")
+
+data17 <- city_data%>%
+  select(c(common_vars, str_which(colnames(city_data), "2017")))%>% mutate (year = 2017)
+colnames(data17)<-c(common_vars, short_vars, "year")
+
+data18 <- city_data%>%
+  select(c(common_vars, str_which(colnames(city_data), "2018")))%>%mutate(year = 2018)
+colnames(data18)<-c(common_vars, short_vars, "year")
+
+dfs <- lst(data13, data14, data15, data16, data17, data18)
+
+cities_long <- bind_rows(dfs)
 
 mypal <- colorNumeric(
   palette = "Spectral",
